d073f4203c97ef6678ac71c1282da8754851e56e
mcabrol <mcabrol.s19@gmail.com>
2020-10-06 10:28:19 +0200

    Have problem on passing object to constants data kernel

---
+++ intersect.js
@@ -0,0 +1,8 @@
+function intersect(ray_direction, ray_origin, id) {
+	let s = vector_substract(ray_direction, ray_origin);
+	return (-1);
+};
+
+let intersect_functions = [
+	intersect
+];
--- vector.js
+++ vector.js
@@ -1,21 +1,26 @@
-function add(a, b) {
+function vector_add(a, b) {
 	return ([a[0] + b[0], a[1] + b[1], a[2] + b[2]]);
 };
 
-function substract(a, b) {
+function vector_substract(a, b) {
 	return ([a[0] - b[0], a[1] - b[1], a[2] - b[2]]);
 };
 
-function multi(a, b) {
+function vector_multi(a, b) {
 	return ([a[0] * b[0], a[1] * b[1], a[2] * b[2]]);
 };
 
-function multin(a, b, n) {
+function vector_multi_n(a, n) {
 	return ([a[0] * n, a[1] * n, a[2] * n]);
 };
 
-function normalize(a) {
-	var x = 1 / sqrt(a[0] * a[0] + a[1] * a[1] + a[2] * a[2]);
+function vector_divide_n(a, n) {
+	var x = 1 / n;
+	return ([a[0] * x, a[1] * x, a[2] * x]);
+};
+
+function vector_normalize(a) {
+	var x = 1 / Math.sqrt(a[0] * a[0] + a[1] * a[1] + a[2] * a[2]);
 	return ([a[0] * x, a[1] * x, a[2] * x]);
 };
 
@@ -23,7 +28,12 @@ function vector_cross(a, b) {
 	return ([
 		a[1] * b[2] - a[2] * b[1],
 		a[2] * b[0] - a[0] * b[2],
+		a[0] * b[1] - a[1] * b[0]
 	]);
+};
+
+function vector_minus(a) {
+	return ([-a[0], -a[1], -a[2]]);
 };
 
 function erand() {
@@ -37,13 +47,14 @@ function degreeToRadian(degree) {
 	return (degree * Math.PI / 180.0)
 }
 
-let kernel_functions = [
-	add,
-	substract,
-	normalize,
-	multi,
-	multin,
+let vector_functions = [
+	vector_add,
+	vector_substract,
+	vector_normalize,
+	vector_multi,
+	vector_multi_n,
+	vector_divide_n,
 	vector_cross,
-	erand,
-	degreeToRadian
+	vector_minus,
+	erand
 ];
