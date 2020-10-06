9556324f6976c0bc790044887f797be155101477
Bob <Bob@Bobs-iMac.local>
2020-10-06 07:59:24 +0200

    Add some vector manipulation func and include a start/stop animation button

--- utils.js
+++ utils.js
@@ -2,3 +2,16 @@ function output(str, element) {
 	element.empty();
 	element.append(str);
 }
+
+function toggleAnimation() {
+	if (animation) {
+		animation = false;
+		$("#statusAnimation").empty();
+		$("#statusAnimation").append("Start");
+	} else {
+		animation = true;
+		$("#statusAnimation").empty();
+		$("#statusAnimation").append("Stop");
+		requestAnimationFrame(draw);
+	}
+}
--- vector.js
+++ vector.js
@@ -6,23 +6,44 @@ function substract(a, b) {
 	return ([a[0] - b[0], a[1] - b[1], a[2] - b[2]]);
 };
 
+function multi(a, b) {
+	return ([a[0] * b[0], a[1] * b[1], a[2] * b[2]]);
+};
+
+function multin(a, b, n) {
+	return ([a[0] * n, a[1] * n, a[2] * n]);
+};
+
 function normalize(a) {
 	var x = 1 / sqrt(a[0] * a[0] + a[1] * a[1] + a[2] * a[2]);
 	return ([a[0] * x, a[1] * x, a[2] * x]);
 };
 
-function getX() {
-	return (this.thread.x);
+function vector_cross(a, b) {
+	return ([
+		a[1] * b[2] - a[2] * b[1],
+		a[2] * b[0] - a[0] * b[2],
+	]);
 };
 
-function getY() {
-	return (this.thread.y);
+function erand() {
+    var min = 0.0;
+	var max = 1.0;
+	var interval = Math.random() * (max - min) + min;
+    return(interval);
 };
+
+function degreeToRadian(degree) {
+	return (degree * Math.PI / 180.0)
+}
 
 let kernel_functions = [
 	add,
 	substract,
 	normalize,
-	getX,
-	getY
+	multi,
+	multin,
+	vector_cross,
+	erand,
+	degreeToRadian
 ];
